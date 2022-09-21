fdea6bf912c6d226b174208b88df25e681bee47c
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-21 14:21:15 -0400

    commit tree 41c621d95af2ec2af1ff46f9235dcefb0ad1221c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent += "Lexie is the champion";
+
+// var paragraph = document.getElementById("p");
+
+// paragraph.textContent += "This just got added";
